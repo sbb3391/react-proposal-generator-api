5993dd8894228c028723fed777a20e655290cac5
Sam Bequette <sbb3391@gmail.com>
2021-10-16 17:36:07 -0400

    allows heroku in cors middleware

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -10,4 +10,9 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     origins 'http://localhost:3001'
     resource '*', headers: :any, methods: [:get, :post, :patch, :put]
   end
+
+  allow do
+    origins 'https://proposals-api.herokuapp.com'
+    resource '*', headers: :any, methods: [:get, :post, :patch, :put]
+  end
 end

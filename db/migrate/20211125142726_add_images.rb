d6dcc3ccdfbc6234428f01939e3d796924182a73
Sam Bequette <sbb3391@gmail.com>
2021-11-25 09:28:16 -0500

    creates images table

---
+++ db/migrate/20211125142726_add_images.rb
@@ -0,0 +1,9 @@
+class AddImages < ActiveRecord::Migration[6.1]
+  def change
+    create_table :images do |t|
+      t.string :image_key
+      t.string :image_url
+      t.timestamps
+    end
+  end
+end

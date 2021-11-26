a28ac3f9919005c0f028bc264ccd77b624c26008
Sam Bequette <sbb3391@gmail.com>
2021-11-26 17:42:57 -0500

    sets up images to be loaded into s3

--- db/migrate/20211125142726_add_images.rb
+++
@@ -1,9 +0,0 @@
-class AddImages < ActiveRecord::Migration[6.1]
-  def change
-    create_table :images do |t|
-      t.string :image_key
-      t.string :image_url
-      t.timestamps
-    end
-  end
-end
